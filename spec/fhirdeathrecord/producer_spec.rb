c7b398db399a20fb276cca01648fb99320bfd76d
holmesie <aholmes@mitre.org>
2018-05-01 21:55:35 -0400

    Fixed tricky date issue in tests

--- spec/fhirdeathrecord/producer_spec.rb
+++ spec/fhirdeathrecord/producer_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe FhirDeathRecord::Producer do
     source = FhirDeathRecord::Producer.to_fhir({'contents': contents, id: '1'}).to_xml.to_s
     source = ignore_urn_uuid(Nokogiri::XML(source).to_xml)
     fixture = ignore_urn_uuid(File.read('spec/fixtures/fhir/1.xml'))
-    expect(source).to eq(fixture)
+    expect(source.gsub(/<date value=".*?"\/>/, '')).to eq(fixture.gsub(/<date value=".*?"\/>/, ''))
   end
   it 'converts nightingale record to SDR FHIR XML for fixture 2' do
     contents = JSON.parse(File.read('spec/fixtures/internal/2.json'))
     source = FhirDeathRecord::Producer.to_fhir({'contents': contents, id: '2'}).to_xml.to_s
     source = ignore_urn_uuid(Nokogiri::XML(source).to_xml)
     fixture = ignore_urn_uuid(File.read('spec/fixtures/fhir/2.xml'))
-    expect(source).to eq(fixture)
+    expect(source.gsub(/<date value=".*?"\/>/, '')).to eq(fixture.gsub(/<date value=".*?"\/>/, ''))
   end
 end
